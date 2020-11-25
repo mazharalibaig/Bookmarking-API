30d238e1f5a48d7aca58e44f2c23d6e358bd0dd9
Mazhar Ali Baig <2018145@iiitdmj.ac.in>
2020-11-25 22:30:57 +0530

    html-css setup

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 
 // mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true});
 
+var data = [{link:'www.xyz.com',title:'asdfsa'},{link:'www.abc.com',title:'gvbhio'}];
+
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 module.exports = function(app){
@@ -12,6 +14,10 @@ module.exports = function(app){
     app.get('/', (req,res) => {
 
         console.log("ALL IS WELL!!\n");
+
+        // res.write(data[0].link);
+
+        res.render('bookmarks',{bookmarks: data});
 
         res.end('Sab Changa Si!\n');
 
